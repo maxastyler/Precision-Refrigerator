4a25decab0dfac6be487f3332aaf8b5e9309e1cb
Max Tyler <mtyler88@gmail.com>
2016-11-07 21:15:21 +0000

    Change server to respond to temperature messages

--- FridgeServer.py
+++ FridgeServer.py
@@ -1,20 +1,40 @@
 import daemon
+import sys
 import socket
+import struct
+import argparse
+
+FRIDGE_PORT=10000
+MESSAGE_SIZE=16
+INITIAL_TARGET_TEMP=10
+usage_string="Usage:\nstart - start/restart the daemon\n(halt/quit/close) - halt the daemon"
+
+def send_message(message):
+    sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address=('localhost', FRIDGE_PORT)
+    try:
+        sock.connect(server_address)
+    except:
+        sock.close()
+        return -1
+    try:
+        sock.sendall('{}'.format(message).encode())
+    finally:
+        sock.close()
+        return 0
 
 class FridgeServer:
     def __init__(self):
         self.running=False
+        self.current_temp=0
+        self.target_temp=INITIAL_TARGET_TEMP
 
     def get_message(self, connection):
-        data_str=""
         try:
-            while True:
-                data=connection.recv(4)
-                data_str+=data.decode('UTF-8')
-                if not data or data_str.find("$end$")!=-1:
-                    break
-        finally: connection.close()
-        return data_str.strip("$end$")
+            data=connection.recv(MESSAGE_SIZE)
+            return data
+        except:
+            return "err"
 
     def quit(self):
         self.running=False
@@ -22,17 +42,50 @@ class FridgeServer:
     def run(self):
         self.running=True
         sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address=('localhost', 10000)
-        sock.bind(server_address)
-        
-        sock.listen(1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        server_address=('localhost', FRIDGE_PORT)
+        try:
+            sock.bind(server_address)
+            
+            sock.listen(1)
 
-        while self.running:
-            connection, client_address=sock.accept()
-            message=self.get_message(connection)
-            if message=='halt' or message=='quit' or message=='close':
-                self.quit()
+            while self.running:
+                message=""
+                connection, client_address=sock.accept()
+                try:
+                    data=self.get_message(connection)
+                    try:
+                        message=data.decode('UTF-8')
+                    except (UnicodeDecodeError, AttributeError) as e:
+                        message="temp"
+                    if message=='stop':
+                        self.quit()
+                    elif message=='gct':
+                        connection.sendall(struct.pack('f', self.current_temp))
+                    elif message=='gtt':
+                        connection.sendall(struct.pack('f', self.target_temp))
+                    else:
+                        self.target_temp=struct.unpack('f', data)[0]
+                finally:
+                    connection.close()
+        finally:
+            sock.close()
 
-with daemon.DaemonContext():
-    a=FridgeServer()
-    a.run()
+    def daemonise():
+        with daemon.DaemonContext():
+            a=FridgeServer()
+            a.run()
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser(description='Daemon to control Refrigerator')
+    parser.add_argument('option', choices=['start', 'stop'], help='Option to give the daemon. Can be start or stop')
+    parser.add_argument('--no-daemon', '-nd', action='store_true', help='Flag given when starting to keep the process in the terminal. No forking')
+    args=parser.parse_args()
+    if args.option=="start":
+        if not args.no_daemon:
+            FridgeServer.daemonise()
+        else:
+            a=FridgeServer()
+            a.run()
+    elif args.option=="stop":
+        send_message("stop")
