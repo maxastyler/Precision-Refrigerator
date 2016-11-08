056423f47e67e2af80761b4a22079ccacc65f64c
Max Tyler <mtyler88@gmail.com>
2016-11-08 11:35:11 +0000

    Add toggleable graph of temperature against time to fridgetk

--- FridgeTK.py
+++ FridgeTK.py
@@ -1,25 +1,83 @@
 #!/usr/bin/env python3
 import tkinter as tk
 from FridgeClient import *
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+plt.ion()
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+from time import time
 
 #Interval with which to update the GUI values
 UPDATE_INTERVAL=1000
 errors={}
 errors[-1]="Connection Error (Is FridgeServer running?)"
 
+
+class GraphFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.x_values=[]
+        self.y_values=[]
+        self.x_t_values=[]
+        self.y_t_values=[]
+        self.start_time=time()
+        self.init_UI()
+
+    def init_UI(self):
+        self.grid_rowconfigure(2, weight=1)
+        self.grid_columnconfigure(3, weight=1)
+        self.f = Figure(figsize=(5,5), dpi=100)
+        self.a = self.f.add_subplot(111)
+        self.line1,=self.a.plot(self.x_values, self.y_values)
+        self.line2,=self.a.plot(self.x_t_values, self.y_t_values)
+        self.canvas = FigureCanvasTkAgg(self.f, self)
+        self.canvas.show()
+        self.canvas.get_tk_widget().grid(row=0, column=0, columnspan=3)
+        self.b={}
+        self.b['clear']=tk.Button(self, text='Reset', command=self.reset_graph)
+        self.b['clear'].grid(row=1, column=0, columnspan=3)
+
+    def update_graph(self):
+        self.line1.set_ydata(self.y_values)
+        self.line1.set_xdata(self.x_values)
+        self.line2.set_ydata(self.y_t_values)
+        self.line2.set_xdata(self.x_t_values)
+        self.a.relim()
+        self.a.autoscale()
+        self.f.canvas.draw()
+
+    def reset_graph(self):
+        self.y_values.clear()
+        self.x_values.clear()
+        self.x_t_values.clear()
+        self.y_t_values.clear()
+        self.start_time=time()
+        self.update_graph()
+
 class App(tk.Frame):
     def __init__(self, root):
         self.root=root
         super().__init__(root)
         self.init_UI()
+        self.graph_hidden=True
 
     def init_UI(self):
         self.root.title="FridgeTK"
         self.pack(fill=tk.BOTH, expand=True)
 
-        self.grid_rowconfigure(3, weight=1)
+        self.grid_rowconfigure(5, weight=1)
         self.grid_columnconfigure(3, weight=1)
 
+        self.frame1()
+
+        self.update_values()
+
+
+    def frame1(self):
         self.labels={}
 
         self.labels['cur_temp_text']=tk.Label(self, text='Current Temperature: ')
@@ -41,9 +99,19 @@ class App(tk.Frame):
         self.entry.grid(row=2, column=1)
         self.button=tk.Button(self, text='Set New Target Temperature', command=self.set_temperature)
         self.button.grid(row=2, column=2)
+        self.graph_frame=GraphFrame(self)
+        self.graph_frame.grid(row=4, column=0, columnspan=3)
+        self.graph_frame.grid_remove()
+        self.show_graph=tk.Button(self, text='Show Graph', command=self.toggle_graph)
+        self.show_graph.grid(row=3, column=0, columnspan=3)
 
-        self.update_values()
-
+    def toggle_graph(self):
+        if self.graph_hidden:
+            self.graph_frame.grid()
+        else:
+            self.graph_frame.grid_remove()
+        self.graph_hidden = not self.graph_hidden
+        
     def loop(self):
         self.root.mainloop()
 
@@ -62,10 +130,15 @@ class App(tk.Frame):
             self.labels['cur_temp_val']['text']=errors[-1]
         else:
             self.labels['cur_temp_val']['text']=float(cur_temp)
+            self.graph_frame.x_values.append(time()-self.graph_frame.start_time)
+            self.graph_frame.y_values.append(float(cur_temp))
         if cur_target_err!=0:
             self.labels['cur_target_val']['text']=errors[-1]
         else:
             self.labels['cur_target_val']['text']=float(cur_target)
+            self.graph_frame.x_t_values.append(time()-self.graph_frame.start_time)
+            self.graph_frame.y_t_values.append(float(cur_target))
+        self.graph_frame.update_graph()
         self.root.after(UPDATE_INTERVAL, self.update_values)
 
     def validate(self, action, index, value_if_allowed,
